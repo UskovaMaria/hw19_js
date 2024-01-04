0dd45945e2ee3e4be930b65beb42b90a8bdad8e6
Uskova Maria <uskovamaria16@gmail.com>
2024-01-04 21:11:17 +0100

    rendering basket

--- js/commonElements.js
+++ js/commonElements.js
@@ -2,6 +2,6 @@ const doc = document;
 const cartEl = doc.querySelector('.cart');
 console.log(cartEl);
 const cartItemsCountEl = cartEl.querySelector('span:last-child');
-
+console.log(cartItemsCountEl);
 const productsSelector = '.products';
 const mainClass = 'product';
--- js/functions/renderCart.js
+++ js/functions/renderCart.js
@@ -1,18 +1,52 @@
 cartEl.onclick = function() {
+
   renderCart();
 }
 
 function renderCart() {
+  const cartContainer = document.querySelector('.cart-container');
+  const cartProductsContainer = document.querySelector('.cart-products');
+
+  cartProductsContainer.innerHTML = '';
+
   const formatCart = formatCartData(cart);
-  console.log(formatCart);
+
+  formatCart.forEach(productId => {
+    const product = products.find(item => item.id === productId);
+
+    if (product) {
+      renderProductInCart(cartProductsContainer, product);
+    }
+  });
+
+  cartContainer.style.display = 'block';
+  
+  const closeCartBtn = document.querySelector('.close-cart-btn');
+  if (closeCartBtn) {
+    closeCartBtn.addEventListener('click', function () {
+      cartContainer.style.display = 'none';
+    });
+  }
+ 
 }
 
-/*
+function renderProductInCart(parentEl, dataObj) {
+  const productInCart = document.createElement('div');
+  productInCart.className = 'cart-product';
 
-1. img Product1 1 x 100 = 100  x
-2. img Product2 3 x 200 = 600  x
-3. img Product3 1 x 300 = 300  x
-----------------------------
-total: 600
+  const cartPhoto = document.createElement('div');
+  cartPhoto.className = 'cart-product__photo';
+  cartPhoto.innerHTML = dataObj.img;
 
-*/
+  const cartName = document.createElement('p');
+  cartName.className = 'cart-product__name';
+  cartName.textContent = dataObj.text;
+
+  const cartPrice = document.createElement('p');
+  cartPrice.className = 'cart-product__price';
+  cartPrice.textContent = dataObj.price;
+
+  productInCart.append(cartPhoto, cartName, cartPrice);
+  parentEl.appendChild(productInCart);  
+}
+
